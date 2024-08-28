68bf9af0cd21efc2fd76ce456aaf14b67c328aca
ksatish369 <satishk.tekion@gmail.com>
2024-08-28 10:56:46 +0530

    added refresh

--- src/main/java/com/ext/chatifyinternalapigateway/controller/routes.java
+++ src/main/java/com/ext/chatifyinternalapigateway/controller/routes.java
@@ -1,18 +1,21 @@
 package com.ext.chatifyinternalapigateway.controller;
 
+import lombok.Data;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+
 @RestController
 @RequestMapping("/routes")
 public class routes {
 
+
     @GetMapping("/refresh")
     public String refresh() {
-        return "Refreshed";
+        return "refresh";
     }
-
-
 }
